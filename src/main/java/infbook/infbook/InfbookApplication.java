10dde35d32089365df9a292e95b40d9f09ea4cc8
goodchoi <jangu3384@gmail.com>
2023-05-31 19:42:07 +0900

    Initialize Spring boot project

---
+++ src/main/java/infbook/infbook/InfbookApplication.java
@@ -0,0 +1,13 @@
+package infbook.infbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/infbook/infbook/InfbookApplicationTests.java
@@ -0,0 +1,13 @@
+package infbook.infbook;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
